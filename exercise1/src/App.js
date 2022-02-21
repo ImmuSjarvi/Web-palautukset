283f0ebb5608ee2bc94e3ca7fb17827c2d4174a8
Immu Saarijärvi <you@example.com>
2022-02-21 09:12:51 +0200

    exercise1 siistimistä

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -4,29 +4,12 @@ import PaivanTimantit from './components/PaivanTimantit';
 import MainPart from './components/MainPart';
 import LuetuimmatContainer from './components/LuetuimmatContainer';
 import {useState} from 'react';
-/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'*/
-/*import {faCaretDown} from */
+
 
 
 
 function App() {
 
-  //Yksin kertanen laskuri nappeilla:  
-  //alla oleva rivi jää tähän
-  //const [ counterValue, setCounterValue] = useState(0);
-
-  //alla olevat rivit pitää olla returnin sisällä:
-  //<div>Laskuri: {counterValue}</div>
-  //<button onClick={ () => setCounterValue(counterValue + 1)}>Increase counter</button>
-  //<button onClick={ () => setCounterValue(counterValue - 1)}>Decrease counter</button>
-  //laskuri loppu
-
-  //Haetaan palvelimelta ostoslistan sisältö
-  /*const shoppingListItems = [
-    'Maito',
-    'Leipä',
-    'Juustoa'
-  ];*/
 
   return (
     <div className="App">
